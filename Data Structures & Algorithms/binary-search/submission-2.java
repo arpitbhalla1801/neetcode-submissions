905d4e0fd5d7fa623bc65578e71d2b42379403aa
Arpit Bhalla <64216681+arpitbhalla1801@users.noreply.github.com>
2026-07-05 12:06:12 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l=0;
+        int r=nums.length-1;
+        int mid = (l+r)/2;
+        while(l<=r) {
+            System.out.println("l="+l);
+            System.out.println("r="+r);
+            System.out.println("mid="+mid);
+            if(target > nums[mid]) {
+                l=mid+1;
+            } else if(target < nums[mid]) {
+                r=mid-1;
+            } else {
+                return mid;
+            }
+            mid=(l+r)/2;
+        }
+        return -1;
+    }
+}
