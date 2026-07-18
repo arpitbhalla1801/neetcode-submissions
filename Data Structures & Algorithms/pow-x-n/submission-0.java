31c52cc7e4257b5ea471615e2d0997d5d565175a
Arpit Bhalla <64216681+arpitbhalla1801@users.noreply.github.com>
2026-07-18 12:43:52 +0530

    Add: pow-x-n - submission-0

---
+++ Data Structures & Algorithms/pow-x-n/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double res = 1;
+        long n1=0;
+        boolean neg = false;
+        n1=Math.abs(n);
+        if(n<0) {
+            neg = true;
+        }
+        while(n1!=0) {
+            if((n1&1)==1) res = res * x;
+            x=x*x;
+            n1 >>= 1;
+        }
+        return neg ? 1/res:res;
+    }
+}
