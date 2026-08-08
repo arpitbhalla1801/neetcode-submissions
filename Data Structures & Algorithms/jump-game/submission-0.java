aca58c46ea71644158a4c0a9696ca1cb89250693
Arpit Bhalla <64216681+arpitbhalla1801@users.noreply.github.com>
2026-08-08 17:13:33 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                System.out.println(i);
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}
